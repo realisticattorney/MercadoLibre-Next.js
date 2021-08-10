56a1e5e968e62be5425ad2f2cf249d99ff8a3000
realisticattorney <g.axl.aquila@gmail.com>
2021-08-10 02:22:21 -0300

    Style ProductFeed using grid (add variation to item's col-spans and add banner)

--- .history/src/components/ProductFeed_20210810021713.js
+++ .history/src/components/ProductFeed_20210810021713.js
@@ -4,23 +4,18 @@ import Product from './Product';
 //With Next you hit this other app, it loads a mid-server that does the work and renders everything (server-side) then delivers the html to the browser
 const ProductFeed = ({ products }) => {
   return (
-    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-40 lg:-mt-52 mx-auto">
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-40 mx-auto">
       {products.slice(0,4).map((product) => (
         <Product key={product.id} product={product} />
       ))}
 
     <img className="md:col-span-full" src="https://links.papareact.com/dyz" alt="" />
       
-      <div className="md:col-span-2">
+      
       {products.slice(4,5).map((product) => (
         <Product key={product.id} product={product} />
       ))}
-      </div>
 
-      {products.slice(5,-1).map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
-      
     </div>
   );
 };
--- .history/src/components/ProductFeed_20210810022017.js
+++ .history/src/components/ProductFeed_20210810022017.js
@@ -4,7 +4,7 @@ import Product from './Product';
 //With Next you hit this other app, it loads a mid-server that does the work and renders everything (server-side) then delivers the html to the browser
 const ProductFeed = ({ products }) => {
   return (
-    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-40 lg:-mt-52 mx-auto">
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-40 md:-mt-40 mx-auto">
       {products.slice(0,4).map((product) => (
         <Product key={product.id} product={product} />
       ))}
